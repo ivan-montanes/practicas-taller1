ce2074f7c8a3779dfedd72faff55a78ded8c46a0
ivan montanes <ivanmontanesfilipit@gmail.com>
2024-11-21 18:52:59 -0300

    pregunta sobre la representacion de cadenas de caracteres 1ra parte respondida

--- README.md
+++ README.md
@@ -16,3 +16,7 @@ las secuencias de escape en un string nos permiten basicamente poder imprimir ci
 - SIGNO DE INTERROGACION LITERAL: \?
 - CARACTER ASCII EN NOTACION OCTAL: \ooo
 - CARACTER ASCII EN NOTACION HEXADECIMAL: \x hh
+## ¿Que sucede cuando utiliza el caracter @ y $ antes de una cadena de texto?
+### ¿Que sucede cuando utiliza el caracter @ antes de una cadena de texto?
+cuando se utiliza el caracter @ antes de una cadena de texto, estamos especificando que queremos que la cadena se interprete y se muestre tal como la escribimos, es decir que las secuencias de escape como \n o \t se interpretaran y mostraran literalmente, a excepcion de una secuencia de escape "" que generara una comilla doble.
+### ¿Que sucede cuando utiliza el caracter $ antes de una cadena de texto?
--- tp-1-ejercicio1/Program.cs
+++ tp-1-ejercicio1/Program.cs
@@ -1,19 +1,2 @@
-﻿//pedimos al usuario que ingrese un numero//
-Console.WriteLine("ingrese un numero");
-string numeroCadena = Console.ReadLine();
-//invertimos la cadena//
-//verificamos que la cadena pueda convertirse a numero en caso contrario mostramos un mensaje de error//
-int numero;
-bool resultadoConvertir = int.TryParse(numeroCadena,out numero);
-if(resultadoConvertir){
-    int cociente, resto, numeroInvertido = 0;
-    do{
-        cociente = numero / 10;
-        resto = numero % 10;
-        numero = cociente;
-        numeroInvertido = (numeroInvertido * 10) + resto;
-    }while(cociente != 0);
-    Console.WriteLine("numero invertido: "+numeroInvertido);
-}else{
-    Console.WriteLine("no se ingreso una cadena en formato numerico");
-}
+﻿string cadena = @"cadena de caracteres  ";
+Console.WriteLine(cadena);
